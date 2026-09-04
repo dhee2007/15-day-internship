41bc1bac800c09f42db2343f820abd64944ac3b3
M Dheeptha <dheeptha2007@gmail.com>
2026-09-04 12:51:04 +0530

    Update script.js

--- CAFE/script.js
+++ CAFE/script.js
@@ -15,3 +15,23 @@ if (themeButton) {
     });
 
 }
+// CONTACT FORM
+
+const contactForm = document.getElementById("contact-form");
+const successMessage = document.getElementById("success-message");
+
+if (contactForm) {
+
+    contactForm.addEventListener("submit", function(event) {
+
+        event.preventDefault();
+
+        successMessage.textContent = "Message sent successfully! ❤️";
+        successMessage.style.color = "#c85a17";
+        successMessage.style.fontWeight = "bold";
+
+        contactForm.reset();
+
+    });
+
+}
